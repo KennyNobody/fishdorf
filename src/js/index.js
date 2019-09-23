59a46e6e9d610b86ab2700e8e19b307a7ede8338
kenny_nobody <nobodykenny@gmail.com>
2019-09-23 22:59:17 +0500

    добавил вкладки отелей

--- src/js/index.js
+++ src/js/index.js
@@ -59,6 +59,66 @@ import Swiper from 'swiper';
 			}
 		});
 	})();
-})();
 
-console.log('Привет')
+	(function changeRoomTabs () {
+		const roomTabs = document.querySelectorAll('.front-room__tab');
+		const roomBlocks = document.querySelectorAll('.front-room__block');
+
+		if (roomTabs && roomBlocks) {
+			let activeTab;
+			for (let i = 0; i < roomTabs.length; i++) {
+				roomTabs[i].addEventListener('click', function() {
+					activeTab = i;
+					toggleLink(activeTab);
+					toggleTab(activeTab);
+					
+				});
+			}
+			
+			function toggleLink (activeTab) {
+				for(let n = 0; n < roomTabs.length; n++) {
+					roomTabs[n].classList.remove('front-room__tab--active');
+				}
+				roomTabs[activeTab].classList.add('front-room__tab--active');
+			}
+
+			function toggleTab(activeTab) {
+				for (let z = 0; z < roomBlocks.length; z++) {
+					roomBlocks[z].classList.add('front-room__block--hidden');
+				}
+				roomBlocks[activeTab].classList.remove('front-room__block--hidden');
+			}
+		}
+	})();
+
+	(function initRoomSliders() {
+		const roomSliders = document.querySelectorAll('.front-room__swiper-container');
+
+		for (let i = 0; i < roomSliders.length; i++) {
+			new Swiper (roomSliders[i], {
+				loop: false,
+				slidesPerView: 3,
+				spaceBetween: 20,
+				navigation: {
+					nextEl: '.front-news__next',
+					prevEl: '.front-news__prev',
+					disabledClass: 'front-news__btn--disabled'
+				},
+				breakpoints: {
+					500: {
+						slidesPerView: 1,
+						spaceBetween: 0
+					},
+					480: {
+						slidesPerView: 2,
+						spaceBetween: 20
+					},
+					900: {
+						slidesPerView: 3,
+						spaceBetween: 20
+					}
+				}
+			});
+		}
+	})();
+})();
